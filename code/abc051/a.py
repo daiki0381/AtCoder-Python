00b405a4265ffab20c3f37cc23c75347d3f010e6
daiki0381 <daiki03810218@gmail.com>
2022-12-11 14:41:29 +0900

    add code

---
+++ code/abc051/a.py
@@ -0,0 +1,10 @@
+# 問題 https://atcoder.jp/contests/abc051/tasks/abc051_a
+
+# s = input().replace(",", " ")
+# print(s)
+
+# s = input().split(",")
+# print(" ".join(s))
+
+s1, s2, s3 = input().split(",")
+print(s1, s2, s3)
