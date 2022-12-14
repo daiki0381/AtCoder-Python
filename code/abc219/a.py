44f6aa527dc1a7c7618708575b5f6e97bdc386a3
daiki0381 <daiki03810218@gmail.com>
2022-12-14 21:00:27 +0900

    add code

---
+++ code/abc219/a.py
@@ -0,0 +1,11 @@
+# https://atcoder.jp/contests/abc219/tasks/abc219_a
+
+X = int(input())
+if X < 40:
+    print(40 - X)
+elif X < 70:
+    print(70 - X)
+elif X < 90:
+    print(90 - X)
+else:
+    print("expert")
