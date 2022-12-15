19aab219fb8c242b998b0b6e912e99beb17ff765
daiki0381 <daiki03810218@gmail.com>
2022-12-15 21:01:17 +0900

    add code

---
+++ code/abc065/a.py
@@ -0,0 +1,9 @@
+# https://atcoder.jp/contests/abc065/tasks/abc065_a
+
+X, A, B = map(int, input().split())
+if A >= B:
+    print("delicious")
+elif B - A <= X:
+    print("safe")
+else:
+    print("dangerous")
