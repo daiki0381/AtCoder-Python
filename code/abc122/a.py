b1b0a23e797c215ef767ddc422493912d2632326
daiki0381 <daiki03810218@gmail.com>
2022-12-16 18:30:48 +0900

    add code

---
+++ code/abc122/a.py
@@ -0,0 +1,11 @@
+# https://atcoder.jp/contests/abc122/tasks/abc122_a
+
+b = input()
+if b == "A":
+    print("T")
+elif b == "T":
+    print("A")
+elif b == "C":
+    print("G")
+else:
+    print("C")
