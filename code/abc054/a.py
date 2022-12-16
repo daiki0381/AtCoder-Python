7b5937db251dee7c79b5f8ceb05cc1879a606f16
daiki0381 <daiki03810218@gmail.com>
2022-12-16 15:14:14 +0900

    add code

---
+++ code/abc054/a.py
@@ -0,0 +1,13 @@
+# https://atcoder.jp/contests/abc054/tasks/abc054_a
+
+A, B = map(int, input().split())
+if A == B:
+    print("Draw")
+elif A == 1:
+    print("Alice")
+elif B == 1:
+    print("Bob")
+elif A > B:
+    print("Alice")
+else:
+    print("Bob")
