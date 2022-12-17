7821a30a4616a8aea49f243204c931f5c7d8caaf
daiki0381 <daiki03810218@gmail.com>
2022-12-17 14:33:38 +0900

    add code

---
+++ code/abc021/a.py
@@ -0,0 +1,25 @@
+# https://atcoder.jp/contests/abc021/tasks/abc021_a
+
+N = int(input())
+print(N)
+for _ in range(N):
+    print(1)
+
+# N = int(input())
+# l = []
+# while N != 0:
+#     if N % 8 == 0:
+#         N -= 8
+#         l.append(8)
+#     elif N % 4 == 0:
+#         N -= 4
+#         l.append(4)
+#     elif N % 2 == 0:
+#         N -= 2
+#         l.append(2)
+#     else:
+#         N -= 1
+#         l.append(1)
+# print(len(l))
+# for i in l:
+#     print(i)
