a970e105a68417e76c2ec0fd9ea43166d1639815
daiki0381 <daiki03810218@gmail.com>
2022-12-19 17:15:34 +0900

    add code

---
+++ code/abc066/a.py
@@ -0,0 +1,7 @@
+# https://atcoder.jp/contests/abc066/tasks/abc066_a
+
+# abc = list(map(int, input().split()))
+# print(sorted(abc)[0] + sorted(abc)[1])
+
+a, b, c = sorted(map(int, input().split()))
+print(a + b)
