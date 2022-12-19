01611ea8ef6183193751cafdb4448041464127f6
daiki0381 <daiki03810218@gmail.com>
2022-12-19 21:53:28 +0900

    add code

---
+++ code/abc188/a.py
@@ -0,0 +1,13 @@
+# https://atcoder.jp/contests/abc188/tasks/abc188_a
+
+X, Y = map(int, input().split())
+if X > Y:
+    if X < Y + 3:
+        print("Yes")
+    else:
+        print("No")
+else:
+    if Y < X + 3:
+        print("Yes")
+    else:
+        print("No")
