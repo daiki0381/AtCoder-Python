fc08d8219cd4abccb671cf1513911e9d9ca1fbea
daiki0381 <daiki03810218@gmail.com>
2022-12-24 15:35:40 +0900

    add code

--- code/abc262/b.py
+++ code/abc262/b.py
@@ -21,13 +21,9 @@ for _ in range(M):
 
 ans = 0
 
-for a in range(1, N + 1):
-    for b in range(a + 1, N + 1):
-        for c in range(b + 1, N + 1):
-            if (
-                table[a - 1][b - 1] == True
-                and table[a - 1][c - 1] == True
-                and table[b - 1][c - 1] == True
-            ):
+for a in range(N):
+    for b in range(a + 1, N):
+        for c in range(b + 1, N):
+            if table[a][b] == True and table[a][c] == True and table[b][c] == True:
                 ans += 1
 print(ans)
