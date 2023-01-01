44735e091dbe545922dc4087c390ffef9d519ba6
daiki0381 <daiki03810218@gmail.com>
2023-01-01 20:09:49 +0900

    add code

---
+++ code/abc214/b.py
@@ -0,0 +1,12 @@
+# https://atcoder.jp/contests/abc214/tasks/abc214_b
+
+S, T = map(int, input().split())
+ans = 0
+
+for a in range(101):
+    for b in range(101):
+        for c in range(101):
+            if a + b + c <= S and a * b * c <= T:
+                ans += 1
+
+print(ans)
