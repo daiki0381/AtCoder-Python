6df6e78088cc0eecea252b1d4e473931ce25fff3
daiki0381 <daiki03810218@gmail.com>
2023-01-03 19:55:13 +0900

    add code

---
+++ code/abc188/b.py
@@ -0,0 +1,15 @@
+# https://atcoder.jp/contests/abc188/tasks/abc188_b
+
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+inner_product = 0
+
+for i in range(N):
+    inner_product += A[i] * B[i]
+
+if inner_product == 0:
+    print("Yes")
+else:
+    print("No")
